c7c6c1391ed5cfca36520d5a8bff7740d27f787b
matryseguros <guimaraes.uenf@gmail.com>
2024-06-19 07:50:59 -0300

    Add files via upload

---
+++ java/index.js
@@ -0,0 +1,3 @@
+function btnClicked(){
+    setTimeout(function () { window.location.href="https://villa.segfy.com/Publico/Segurados/Orcamentos/SolicitarCotacao?e=KL2BucbW1Ptm5UTtDwv%2BQuahPSYUaG2U"; }, 1100);
+}
